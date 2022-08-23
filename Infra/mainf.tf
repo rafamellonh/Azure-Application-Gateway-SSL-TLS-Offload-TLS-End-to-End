20bcc19b476a21314f74efeb467639750725c15e
Rafael <56116868+rafamellonh@users.noreply.github.com>
2022-08-23 17:39:20 -0300

    SSL

--- Infra/mainf.tf
+++ Infra/mainf.tf
@@ -3,3 +3,26 @@ resource "azurerm_resource_group" "RGAPP" {
   location = var.location
   tags = var.tags
 }
+
+resource "azurerm_virtual_network" "VNET01" {
+  name = "VNET01"
+  resource_group_name = azurerm_resource_group.RGAPP.name
+  location = var.location
+  tags = var.tags
+  address_space = [ "10.10.0.0/16" ]
+  
+  subnet = {
+    name = "SUB-APPGW"
+    address_prefix = "10.10.2.0/24"
+  }
+
+  subnet = {
+    name = "SUB-SRV"
+    address_prefix = "10.10.1.0/24"
+  }
+
+  subnet = {
+    name = "AzureBastionSubnet"
+    address_prefix = "10.10.250.0/26"
+  }
+}

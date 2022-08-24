55551c1b819725488128f8810e43d4a153ea6370
Rafael <56116868+rafamellonh@users.noreply.github.com>
2022-08-24 14:10:39 -0300

    SSL

--- Infra/mainf.tf
+++ Infra/mainf.tf
@@ -10,6 +10,18 @@ resource "azurerm_network_security_group" "NSG01" {
   location            = var.location
   resource_group_name = azurerm_resource_group.RGAPP.name
   tags                = var.tags
+
+  security_rule {
+    name                       = "Port80"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "NSG-TO-SUB-SRV" {
--- Infra/output.tf
+++ Infra/output.tf
@@ -1,3 +1,3 @@
-output "private-ip" {
-  value = private_ip_address_allocation.ip_configuration
-}
+#output "private-ip" {
+#  value = private_ip_address_allocation.VM01.ip_configuration
+#}

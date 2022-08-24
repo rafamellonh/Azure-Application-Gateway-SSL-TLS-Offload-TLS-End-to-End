1deedd1e6cb9b4b1e0ba897b651a7d2df1e6c585
Rafael <56116868+rafamellonh@users.noreply.github.com>
2022-08-24 13:04:43 -0300

    SSL

--- Infra/mainf.tf
+++ Infra/mainf.tf
@@ -13,7 +13,7 @@ resource "azurerm_network_security_group" "NSG01" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "NSG-TO-SUB-SRV" {
-  subnet_id = azurerm_subnet.SUB-SRV.id
+  subnet_id                 = azurerm_subnet.SUB-SRV.id
   network_security_group_id = azurerm_network_security_group.NSG01.id
 }
 
@@ -40,8 +40,45 @@ resource "azurerm_subnet" "SUB-APPGW" {
 }
 
 resource "azurerm_subnet" "AzureBastionSubnet" {
-  name = "AzureBastionSubnet"
-  resource_group_name = azurerm_resource_group.RGAPP.name
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.RGAPP.name
   virtual_network_name = azurerm_virtual_network.VNET01.name
-  address_prefixes = [ "10.10.250.0/26" ]
+  address_prefixes     = ["10.10.250.0/26"]
+}
+
+resource "azurerm_network_interface" "NIC-VM01" {
+  name                = "NIC-VM01"
+  resource_group_name = azurerm_resource_group.RGAPP.name
+  location            = var.location
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.SUB-SRV.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# SERVIDORES VIRTUAIS
+resource "azurerm_windows_virtual_machine" "VM01" {
+  name = "VM01"
+  resource_group_name = azurerm_resource_group.RGAPP.name
+  location = var.location
+  tags = var.tags
+  size = "Standard_D2s_V3"
+  admin_username = "rafael.admin"
+  admin_password = "@#abc,123@#"
+  network_interface_ids = [ azurerm_network_interface.NIC-VM01.id, ]
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Premium_ZRS"
+  }
+  
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer = "WindowsServer"
+    sku = "2022-Datacenter"
+    version = "lastest"
+  }
+
 }
